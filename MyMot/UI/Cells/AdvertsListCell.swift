f8344a3e823b48f8d2bb95df29eac1d7d84ab004
Michail Solyanic <solyanic@rambler.ru>
2019-04-02 18:00:41 +0300

    add cells and coreModels

---
+++ MyMot/UI/Cells/AdvertsListCell.swift
@@ -0,0 +1,24 @@
+//
+//  AdvertsListCell.swift
+//  MyMot
+//
+//  Created by Michail Solyanic on 02/04/2019.
+//  Copyright © 2019 Michail Solyanic. All rights reserved.
+//
+
+import UIKit
+
+class AdvertsListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MyMot/UI/Controllers/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  MyMot
+//
+//  Created by Michail Solyanic on 02/04/2019.
+//  Copyright © 2019 Michail Solyanic. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
