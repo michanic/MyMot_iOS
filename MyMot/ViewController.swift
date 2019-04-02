1a11be568afa1457973fd6b640cf9503a5bb5cf3
Mikhail <solyanic@rambler.ru>
2019-04-02 15:38:35 +0300

    Initial Commit

---
+++ MyMot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMot
+//
+//  Created by Michail Solyanic on 02/04/2019.
+//  Copyright © 2019 Michail Solyanic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
